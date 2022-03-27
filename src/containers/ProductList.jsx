271604c41760f41d452e37b2a8297e4489bcef67
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2022-03-26 19:55:53 -0600

    useEffect y consumo de api - Clase #18

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
 
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products/';
+
 const ProductList = () => {
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(async () => {
+		/* uso de fetch */
+		// const response = await fetch(API);
+		// setProducts(await response.json());
+
+		/* uso de axios */
+		const response = await axios(API);
+		console.log(response);
+		setProducts(response.data);
+	}, []);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
