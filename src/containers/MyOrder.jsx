882c13bdaa33ac58848733f561b7bf7f1e5c033a
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2022-04-15 14:53:51 -0500

    Removiendo productos del carrito de compras - Clase#25

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import OrderItem from '../components/OrderItem';
 import AppContext from '../context/AppContext';
 
+import backArrow from '@icons/flechita.svg';
 import '../styles/MyOrder.scss';
 
 const MyOrder = () => {
@@ -18,12 +19,12 @@ const MyOrder = () => {
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
-				<img src="./icons/flechita.svg" alt="arrow" />
+				<img src={backArrow} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				{state.cart.map( (product) => (
-					<OrderItem product={product} key={`OrderItem-${product.id}`}/>
+				{state.cart.map((product) => (
+					<OrderItem product={product} key={`OrderItem-${product.id}`} />
 				))}
 				<div className="order">
 					<p>
