75b4df510ac0c13a7a7ccfa5f511d7f27027a537
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2022-04-15 00:25:57 -0500

    Carrito de compras - Clase#22

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
 
 import Menu from '@components/Menu';
+import AppContext from '../context/AppContext';
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
@@ -13,6 +14,7 @@ import shoppingCart from '@icons/icon_shopping_cart.svg';
 const Header = () => {
 
 	const [toggle, setToggle] = useState(false);
+	const { state } = useContext(AppContext);
 
 	const handleToggle = () => {
 		setToggle(!toggle);
@@ -53,7 +55,7 @@ const Header = () => {
 					</li>
 					<li className="navbar-shopping-cart">
 						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
