ec712e5b898db2e507094d7728ff6cc0edfb5fd6
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2022-04-15 16:17:56 -0500

    [FIX] routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter basename='/practico-react-js-platzi'>
 				<Layout>
 					<Switch>
 						<Route exact path="/" component={Home} />
